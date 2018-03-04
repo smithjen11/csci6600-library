fb3148028df13ea9b6dda952ddab3d8916bacd6b
Jennifer Smith <smithjen11@students.ecu.edu>
2018-03-04 10:59:00 -0500

    Run holds table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304154942) do
+ActiveRecord::Schema.define(version: 20180304155609) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,17 @@ ActiveRecord::Schema.define(version: 20180304154942) do
     t.string   "image_url"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "holds", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "user_id"
+    t.datetime "request_date"
+    t.datetime "release_date"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["book_id"], name: "index_holds_on_book_id"
+    t.index ["user_id"], name: "index_holds_on_user_id"
   end
 
   create_table "loans", force: :cascade do |t|
