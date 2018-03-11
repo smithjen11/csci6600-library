abebeca818424b8a84ff94883b1031909231ad57
Jennifer Smith <smithjen11@students.ecu.edu>
2018-03-11 16:05:11 -0400

    Remove json routes

--- app/controllers/holds_controller.rb
+++ app/controllers/holds_controller.rb
@@ -2,13 +2,11 @@ class HoldsController < ApplicationController
   before_action :set_hold, only: [:show, :edit, :update, :destroy]
 
   # GET /holds
-  # GET /holds.json
   def index
     @holds = Hold.all
   end
 
   # GET /holds/1
-  # GET /holds/1.json
   def show
   end
 
@@ -22,42 +20,34 @@ class HoldsController < ApplicationController
   end
 
   # POST /holds
-  # POST /holds.json
   def create
     @hold = Hold.new(hold_params)
 
     respond_to do |format|
       if @hold.save
         format.html { redirect_to @hold, notice: 'Hold was successfully created.' }
-        format.json { render :show, status: :created, location: @hold }
       else
         format.html { render :new }
-        format.json { render json: @hold.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /holds/1
-  # PATCH/PUT /holds/1.json
   def update
     respond_to do |format|
       if @hold.update(hold_params)
         format.html { redirect_to @hold, notice: 'Hold was successfully updated.' }
-        format.json { render :show, status: :ok, location: @hold }
       else
         format.html { render :edit }
-        format.json { render json: @hold.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /holds/1
-  # DELETE /holds/1.json
   def destroy
     @hold.destroy
     respond_to do |format|
       format.html { redirect_to holds_url, notice: 'Hold was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
