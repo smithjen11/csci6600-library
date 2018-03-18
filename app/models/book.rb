86a8adca17fbb781ae74e1e094582440895293a0
Jennifer Smith <smithjen11@students.ecu.edu>
2018-03-18 18:34:42 -0400

    Add search by genre/category funcitonality

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,22 +6,39 @@ class Book < ApplicationRecord
 
 	def search(term, type)
 		if type == 'author'
-			if term.match(/,/)
-				term = term.split(', ')
-			else
-				term = term.split(' ')
-			end
-			if term[0].blank? || term[1].blank?
-				term = term.reject { |t| t.blank? }
-				Book.where("author_last_name like ? or author_first_name like ?", 
-					'%'+term[0].to_s+'%', '%'+term[0].to_s+'%')
-			else
-				Book.where("(author_last_name like ? and author_first_name like ?) or "+
-					"(author_last_name like ? and author_first_name like ?)", 
-					'%'+term[0].to_s+'%', '%'+term[1].to_s+'%', '%'+term[1].to_s+'%', '%'+term[0].to_s+'%')
-			end
+			author_search(term)
 		elsif type == 'title'
-			Book.where("title like ?", '%'+term.to_s+'%')
+			title_search(term)
+		elsif type == 'genre'
+			genre_search(term)
 		end
 	end
+
+
+	private
+
+	 def author_search(term)
+	   if term.match(/,/)
+	     term = term.split(', ')
+	   else
+	     term = term.split(' ')
+	   end
+	   if term[0].blank? || term[1].blank?
+	     term = term.reject { |t| t.blank? }
+	     Book.where("author_last_name like ? or author_first_name like ?", 
+	     	'%'+term[0].to_s+'%', '%'+term[0].to_s+'%')
+	   else
+	     Book.where("(author_last_name like ? and author_first_name like ?) or "+
+	     	"(author_last_name like ? and author_first_name like ?)", 
+	     	'%'+term[0].to_s+'%', '%'+term[1].to_s+'%', '%'+term[1].to_s+'%', '%'+term[0].to_s+'%')
+	   end
+	 end
+
+	 def title_search(term)
+	   Book.where("title like ?", '%'+term.to_s+'%')
+	 end
+
+	 def genre_search(term)
+	   Book.where(genre: term)
+	 end
 end
