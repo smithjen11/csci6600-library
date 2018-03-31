eba29d27a3bf0b0d7090fff93ede82ebdbae4aa3
Jennifer Smith <smithjen11@students.ecu.edu>
2018-03-31 09:45:59 -0400

    Add js for dropdowns; added dropdown for book actions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
+//= require popper.min
+//= require bootstrap.bundle.min
 //= require jquery.dataTables.min
 //= require_tree .
 
